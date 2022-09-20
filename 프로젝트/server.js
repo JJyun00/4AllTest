c4886be81fd05a6270c154905801a06d6b3c37d0
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-09-20 10:24:47 +0900

    Update server.js

--- 프로젝트/server.js
+++ 프로젝트/server.js
@@ -1,8 +1,25 @@
-var express = require('express')
-var app = express()
+let http = require('http');
+let fs = require('fs');     //파일 읽기, 쓰기 등 할 수 있는 모듈
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/GETest.html');
-})
+//404 error message : 페이지 오류 발생했을때,
+function send404Message(response){
+    response.writeHead(404, {"Content-Type":"text/plain"});     //단순한 글자 출력
+    response.write("404 ERROR...");
+    response.end();
+}
 
-app.listen(3000);
+// 200 Okay : 정상적인 요청
+function onRequest(request, response){
+
+    if(request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, {"Content-Type":"text/html"});      //웹페이지 출력
+        fs.createReadStream("./GETest.html").pipe(response);     //같은 디렉토리에 있는 GETest.html 을 response함
+
+    } else {
+        send404Message(response);       //file이 존재하지 않을 때
+
+    }
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server Created...");
