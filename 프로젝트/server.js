d3fb393feebbaf419e1f770a38f6ac427138f78c
JJyun00 <113875839+JJyun00@users.noreply.github.com>
2022-09-28 16:45:11 +0900

    Update server.js
    mysql 연결

--- 프로젝트/server.js
+++ 프로젝트/server.js
@@ -1,28 +1,48 @@
 // 기본 설정
+const path = require('path')
 const express = require("express");  // 웹서버 사용
-const server = express();
+const bodyParser = require('body-parser')
+const app = express();
+const PORT = 8888;
 
-// 정적 파일 불러오기
-server.use(express.static(__dirname + "/css"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-// 라우팅 정의
-server.get("/", (req, res) => {
+const mysql = require('mysql');
+const { CONNREFUSED } = require('dns');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'jjy',
+  password: 'wldbsdhfkzmf123!',
+  database: 'getest'
+});
+
+
+// 정적 파일 불러오기(css)
+app.use(express.static(__dirname + '/'));
+
+
+// 라우팅 정의(html)
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/GETest.html");
 });
 
-server.get("/CI.html", (req, res) => {
-  res.sendFile(__dirname + "/CI.html");
+
+app.get('/information', (req, res) =>
+  res.sendFile(path.join(__dirname, '/information.html')));
+
+app.post('/information', (req, res) => {
+  const sql = "INSERT INTO info SET ?"
+
+  con.query(sql, req.body, function(err, result, fields){
+    if(err) throw err;
+    console.log(result);
+  });
 });
 
-server.get("/BI.html", (req, res) => {
-  res.sendFile(__dirname + "/BI.html");
-});
-
-server.get("/information.html", (req, res) => {
-  res.sendFile(__dirname + "/information.html");
-});
 
 // 서버 실행
-server.listen(8888, () => {
-  console.log(`Listen : 8888`);
+app.listen(PORT, () => {
+  console.log(`Listen : ${PORT}`);
 });
